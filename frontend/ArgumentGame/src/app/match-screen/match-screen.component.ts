990ef2ce62f001e274248c822b2adda8a18b5867
Tom Löffelmann <tom.spoonman+git@gmail.com>
2023-05-04 10:23:26 +0200

    add basic matchScreen implementation

--- frontend/ArgumentGame/src/app/match-screen/match-screen.component.ts
+++ frontend/ArgumentGame/src/app/match-screen/match-screen.component.ts
@@ -3,13 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-match-screen',
   templateUrl: './match-screen.component.html',
-  styleUrls: ['./match-screen.component.scss']
+  styleUrls: ['./match-screen.component.scss'],
 })
 export class MatchScreenComponent implements OnInit {
+  argument = 'This is a sample argument with multiple statements for testing purposes.';
+  selectedText = '';
+  agreeDisabled = true;
+  disagreeDisabled = true;
+  proposeChangeDisabled = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onTextSelected(event: MouseEvent): void {
+    const selection = window.getSelection();
+    this.selectedText = selection?.toString() || '';
+
+    this.updateActionButtons();
   }
 
+  updateActionButtons(): void {
+    const hasSelection = this.selectedText.length > 0;
+    this.agreeDisabled = !hasSelection;
+    this.disagreeDisabled = !hasSelection;
+    this.proposeChangeDisabled = !hasSelection;
+  }
+
+  onAgree(): void {
+    // Implement the agree action
+  }
+
+  onDisagree(): void {
+    // Implement the disagree action
+  }
+
+  onProposeChange(): void {
+    // Implement the propose change action
+  }
+
+  onCommitChanges(): void {
+    // Implement the commit changes action
+  }
 }
